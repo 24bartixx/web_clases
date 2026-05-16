72342cc530a181c8e1e2dbbceea869d052f36b7f
24bartixx <jestgitbartix@gmail.com>
2026-05-16 18:43:58 +0200

    chore: set up frontend with typescript project

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import { HomePage } from "../pages/home/HomePage.tsx";
-import { LoginPage } from "../pages/login/LoginPage.tsx";
-import { PortfolioPage } from "../pages/portfolio/PortfolioPage.tsx";
-import { TradingViewDemoPage } from "../pages/trading_view_test/TradingViewDemoPage.tsx";
+import { HomePage } from "../pages/home/HomePage";
+import { LoginPage } from "../pages/login/LoginPage";
+import { PortfolioPage } from "../pages/portfolio/PortfolioPage";
+import { TradingViewDemoPage } from "../pages/trading_view_test/TradingViewDemoPage";
 
 export const router = createBrowserRouter([
   {
