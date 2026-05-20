66df7cd32e89d5422f8f5726332b3a73ed0e62fe
Kuba <101518357+jakubbaldyga@users.noreply.github.com>
2026-05-20 13:48:30 +0200

    feat: decoding jwt respone from google oauth

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,10 +1,15 @@
 import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 export function LoginPage() {
   return (
     <GoogleLogin
       onSuccess={(credentialResponse) => {
-        console.log(credentialResponse);
+        const decoded: any = jwtDecode(credentialResponse.credential!);
+        console.log("Imię:", decoded.given_name);
+        console.log("Nazwisko:", decoded.family_name);
+        console.log("Email:", decoded.email);
+        console.log("ID:", decoded.sub);
       }}
       onError={() => {
         console.log("Login Failed");
